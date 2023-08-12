58383a1b9d7178dc4c4c89b3085f7adc1372d259
Noam Koren <noamko25@gmail.com>
2023-08-12 20:42:41 +0300

    Initial commit

---
+++ desgin.md
@@ -0,0 +1,5 @@
+# Design
+> This is a design document for the project. It will be updated as the project progresses.
+
+## Main idea
+
---
+++ home.py
@@ -0,0 +1,16 @@
+import socket
+import os
+import sys
+
+sys.path.append(f'{os.getcwd()}/ui')
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='ui', static_folder='static')
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+    
+if __name__ == "__main__":
+    app.run()
