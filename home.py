a35723ec3064806dd0230b38f76cf7e633cf1c9e
noamk <noamk@bindecy.local>
2024-04-16 21:23:59 +0300

    dunno

--- home.py
+++ home.py
@@ -11,6 +11,10 @@ app = Flask(__name__, template_folder='ui', static_folder='static')
 def index():
     return render_template("index.html")
 
+
+@app.route("/dripitidrop")
+def dripitidrop():
+    return render_template("dripitidrop.html")
     
 if __name__ == "__main__":
     app.run()
